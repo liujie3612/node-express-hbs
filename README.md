8d82eea2c624d9fcdad7be693f49f9ab3e6a11cb
liujie3612 <liujie3612@wilddog.com>
2015-12-19 17:44:22 +0800

    加上bin/www启动项

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * cd node-express-hbs
 * npm install
 * node app.js
-* 127.0.0.1:5000
+* 127.0.0.1:5000;
 
 ## Defective
 
@@ -13,6 +13,4 @@
 2. bower
 3.  route
 
-
-
-<div style="color:rgb(230,80,30)">app.js needs to continue to complement!!</div>
+<div style="color:rgb(230,80,30);">app.js needs to continue to complement!!</div>
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var favicon = require('static-favicon');
 var exphbs = require('express3-handlebars');
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -7,16 +8,45 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-	var Number =  Math.round(Math.random() * 10);
-    res.render('index',{
-    	luckyNumber :  Number 
+    var Number = Math.round(Math.random() * 10);
+    res.render('index', {
+        luckyNumber: Number
     });
 });
-
 app.get('/about', function(req, res) {
     res.render('about');
 })
 app.use('/public', express.static('public'));
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port);
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
