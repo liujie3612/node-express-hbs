d10faf36d43c1f9e58b6d57900326a1d401c773c
liujie3612 <liujie@wilddog.com>
2016-03-15 00:49:44 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var browsersync = require('browser-sync').create();
 var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
@@ -9,6 +10,29 @@ var imagemin = require('gulp-imagemin');
 var minifyCss = require('gulp-minify-css');
 var usemin = require('gulp-usemin');
 var csso = require('gulp-csso');
+/*
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+});*/
+
+gulp.task('clean', function () {
+    return gulp.src('dist/*', {
+            read: false
+        })
+        .pipe(clean());
+});
+
+gulp.task('copy', function () {
+    return gulp.src(['bower_components/**/*.*', 'views/**/*.html','README.md', 'config.js', 'app.js', 'bin/www', 'markdown-toc.js', 'marked.js', 'raneto.js', 'content/**/*', 'package.json', 'public/*.*', 'public/fonts/**/*.*', 'node_modules/**/*.*'], {
+            base: '.',
+            'buffer': false
+        })
+        .pipe(gulp.dest('dist'));
+})
 
 
 gulp.task('build', ['css', 'js', 'image', 'font']);
